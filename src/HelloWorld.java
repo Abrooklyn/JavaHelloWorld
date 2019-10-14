14722e7b19b11e1a40daeb7ab173e22bec1e9e38
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-10-14 08:31:27 +0200

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String... args){
+        System.out.println("Hello World");
+        int anneeDeNaissance=1993;
+        anneeDeNaissance=1994;
+        boolean javaIsEasy=true;
+
+    }
+}
