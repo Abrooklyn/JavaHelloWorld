319aa71ca5b934e93ae0e276ed5e49384e101e8d
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-10-14 11:36:42 +0200

    Operateurs relationels

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,8 @@
 public class HelloWorld {
     public static void main(String... args){
-        System.out.println("Hello World");
-        int anneeDeNaissance=1993;
-        anneeDeNaissance=1994;
-        boolean javaIsEasy=true;
-
+        boolean vraiOuFaux=20==30;
+        System.out.println(vraiOuFaux);
+        boolean fauxOuVrai=20<30;
+        System.out.println(fauxOuVrai);
     }
 }
